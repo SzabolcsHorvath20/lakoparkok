b9740693039a643fb98f66f096398bdf9d59e33e
garru <garruswasab@gmail.com>
2020-03-20 20:35:10 +0100

    Initial commit

--- lakoparkok/Happyliving.cs
+++ lakoparkok/Happyliving.cs
@@ -22,40 +22,40 @@ namespace lakoparkok
 
         public HappyLiving(string filenev)
         {
-            string[] L;
+            string[] split;
 
-            StreamReader R = new StreamReader(filenev);
-            while (! R.EndOfStream)
+            StreamReader read = new StreamReader(filenev);
+            while (!read.EndOfStream)
             {
-                string nev = R.ReadLine();
+                string nev = read.ReadLine();
                 if (nev.Length > 0)
                 {
-                    string[] T = R.ReadLine().Split(';');
-                    int utcaSzam = int.Parse(T[0]);
-                    int maxhazSzam  = int.Parse(T[1]);
-                    int[,] TH = new int[utcaSzam,maxhazSzam];
+                    string[] split2 = read.ReadLine().Split(';');
+                    int utcaSzam = int.Parse(split2[0]);
+                    int maxhazSzam  = int.Parse(split2[1]);
+                    int[,] sor = new int[utcaSzam,maxhazSzam];
                     for (int s= 0; s < utcaSzam; s++)
                     {
                         for (int o = 0; o < maxhazSzam; o++)
                         {
-                            TH[s, o] = 0;
+                            sor[s, o] = 0;
                         }
                     }
                     do
                     {
-                        L = R.ReadLine().Split(';');  
-                        if (L[0] != "") {
-                            TH[ int.Parse(L[0]) - 1 , int.Parse(L[1]) - 1] = 
-                              int.Parse(L[2]);
+                        split = read.ReadLine().Split(';');  
+                        if (split[0] != "") {
+                            sor[ int.Parse(split[0]) - 1 , int.Parse(split[1]) - 1] = 
+                              int.Parse(split[2]);
                         }
 
-                    } while (L[0] != "");
+                    } while (split[0] != "");
 
-                    Lakopark LP = new Lakopark(nev, utcaSzam, maxhazSzam, TH);
+                    Lakopark LP = new Lakopark(nev, utcaSzam, maxhazSzam, sor);
                     lakoparkok.Add(LP);
                 }
             }
-            R.Close();
+            read.Close();
         }
     }
 }
